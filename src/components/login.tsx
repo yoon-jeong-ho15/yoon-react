ca138f2e68355e51cc6f41afd118f30a7e75cf2b
Yoon Jeong Ho <yoonjh1015@gmail.com>
2025-04-18 22:03:07 +0900

    input focus 문제 있음.

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 export default function Login() {
-  const login = () => {};
+  const [username, setUsername] = useState("");
+  const [pwd, setPwd] = useState("");
+  const pwdInputRef = useRef<HTMLInputElement>(null);
+  const usernameInputRef = useRef<HTMLInputElement>(null);
+
+  const usernameInputHandler = (value) => {
+    setUsername(value);
+  };
+
+  const pwdInputHandler = (value) => {
+    const numbersOnly = value.replace(/[^0-9]/g, "");
+    setPwd(numbersOnly);
+  };
+
+  const focusInputEnd = (ref) => {
+    if (ref.current) {
+      ref.current.focus();
+      const length = ref.current.value.length;
+      ref.current.setSelectionRange(length, length);
+    }
+  };
+
+  const login = () => {
+    if (username == "윤정호" && pwd == "941015") {
+      alert("loged in");
+    } else {
+      alert("not logged in");
+    }
+  };
+
   return (
     <div className="flex justify-center">
       <div
@@ -20,10 +49,18 @@ export default function Login() {
         >
           당신은 누구신가요?
         </span>
-        <form className="w-60 h-20 mt-6 mb-3 flex flex-col justify-around">
+        <div className="w-60 h-20 mt-6 mb-3 flex flex-col justify-around">
           <input
             name="username"
             placeholder="이름"
+            value={username}
+            ref={usernameInputRef}
+            onChange={(e) => usernameInputHandler(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter" || e.key === "ArrowDown") {
+                focusInputEnd(pwdInputRef);
+              }
+            }}
             className="
                 bg-gray-50 text-center rounded-md py-1
                 outline-none tracking-widest
@@ -35,6 +72,22 @@ export default function Login() {
           <input
             name="pwd"
             placeholder="생년월일"
+            value={pwd}
+            ref={pwdInputRef}
+            onChange={(e) => pwdInputHandler(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                login();
+              } else if (e.key === "ArrowUp") {
+                focusInputEnd(usernameInputRef);
+                // setTimeout(() => {
+                //   if (usernameInputRef.current) {
+                //     const length = usernameInputRef.current.value.length;
+                //     usernameInputRef.current.setSelectionRange(length, length);
+                //   }
+                // }, 0);
+              }
+            }}
             className="
                 bg-gray-50 text-center rounded-md py-1
                 outline-none tracking-widest
@@ -43,10 +96,10 @@ export default function Login() {
                 transition-all
             "
           ></input>
-        </form>
+        </div>
         <button
           onClick={() => login()}
-          className="w-20 h-6 rounded-md bg-gray-300 hover: active:bg-gray-400"
+          className="w-20 h-6 rounded-md bg-gray-300 hover:bg-gray-200 active:bg-gray-300"
         >
           접속
         </button>
--- src/pages/board.tsx
+++ src/pages/board.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Login from "../components/login";
 import BoardList from "../components/board-list";
 
 export default function Board() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <>
       <div>
         <h1 className="text-5xl">게시판</h1>
       </div>
-      <Login />
+      {isLoggedIn ? <BoardList /> : <Login />}
     </>
   );
 }
